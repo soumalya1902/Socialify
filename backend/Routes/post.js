405b5b93000e8a56b9774dc708b3d8650337324f
Ashish Manna <m.manna.bidyut@gmail.com>
2024-03-05 16:17:24 +0530

    First commit

---
+++ backend/Routes/post.js
@@ -0,0 +1,16 @@
+const express = require("express");
+// import express from "express";
+const { authVerify } = require("../Controllers/authController");
+const {
+  createPost,
+  fetchPosts,
+  addComment,
+} = require("../Controllers/postController");
+const router = express.Router();
+
+router.post("/create-post", authVerify, createPost);
+router.get("/fetch-post", authVerify, fetchPosts);
+
+router.post("/add-comment", authVerify, addComment);
+
+module.exports.postRouter = router;
